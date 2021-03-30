afa19d624a186a446cd370c6123a95e73be090da
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2021-03-30 13:36:06 +0200

    2021-3-30

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,22 @@
-public class Controller {
+import javax.swing.*;
+import java.net.Socket;
 
-    
+public class Controller{
 
+
+
+    public Controller() {
+
+        String ip = (String) JOptionPane.showInputDialog(null,"IP?","Connect to..",JOptionPane.QUESTION_MESSAGE);
+        int port = Integer.parseInt(JOptionPane.showInputDialog(null,"Port?","Connect to..",JOptionPane.QUESTION_MESSAGE));       ;
+        Socket socket = null;
+
+        try {
+            socket = new Socket(ip,port);
+        } catch (Exception e) {
+            System.out.println("Client failed to connect");
+            System.exit(0);
+        }
+
+    }
 }
